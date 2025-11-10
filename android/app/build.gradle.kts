b83dab3545c75ff592069f8150bf100aae6d7684
Thành Nam <trinhthanhnam2003@gmail.com>
2025-11-10 16:04:58 +0700

    release/v1.0.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
 android {
     namespace = "com.felix.bus_ticket_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
