2cec792934779c42637c2495c6463ee58a65376c
Thành Nam <trinhthanhnam2003@gmail.com>
2025-11-18 13:49:19 +0700

    update google-service.json

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.bus_ticket_app"
+    namespace = "com.felix.bus_ticket_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -33,7 +33,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.bus_ticket_app"
+        applicationId = "com.felix.bus_ticket_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
--- android/app/src/main/kotlin/com/example/bus_ticket_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/bus_ticket_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.bus_ticket_app
+package com.felix.bus_ticket_app
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
